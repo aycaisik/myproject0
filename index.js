c9df4483843db272a5bc743f4ce18c4f30ce1a4e
Ayça Işık <117450558+aycaisik@users.noreply.github.com>
2023-03-05 16:15:52 +0300

    Test.

--- index.js
+++ index.js
@@ -3,7 +3,7 @@
  */
 
 import {AppRegistry} from 'react-native';
-import App from './App';
+import myApp from './src/myApp';
 import {name as appName} from './app.json';
 
-AppRegistry.registerComponent(appName, () => App);
+AppRegistry.registerComponent(appName, () => myApp);
---
+++ src/myApp.js
@@ -0,0 +1,18 @@
+import React from "react"
+import {View, Text} from 'react-native'
+
+const myApp = () => {
+    return(
+        <View style={{
+            flex: 1,
+            backgroundColor: 'purple',
+            justifyContent: 'center',
+            alignItems: 'center'
+            
+            
+        }}>
+            <Text>slm ask 💖</Text>
+        </View>
+    )
+}
+export default myApp
